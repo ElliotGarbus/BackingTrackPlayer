13247146ad8a44ba01d93774f764e22fe7d60a1f
ElliotGarbus <elliotg2@cox.net>
2020-11-05 07:19:07 -0700

    added icon

--- configstartup.py
+++ configstartup.py
@@ -7,4 +7,5 @@ This code must be at the top of the 'main' executable file.
 
 Config.set('kivy', 'exit_on_escape', 0)
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
+Config.set('kivy', 'window_icon', 'icons8-refresh-64.png')
 
