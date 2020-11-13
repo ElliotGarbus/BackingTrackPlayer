d85725385c3be1201deeeefd7d51aa7fb23d2373
ElliotGarbus <elliotg2@cox.net>
2020-11-12 18:07:22 -0700

    added reference to icon image

--- configstartup.py
+++ configstartup.py
@@ -7,5 +7,5 @@ This code must be at the top of the 'main' executable file.
 
 Config.set('kivy', 'exit_on_escape', 0)
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
-Config.set('kivy', 'window_icon', 'icons8-refresh-64.png')
+Config.set('kivy', 'window_icon', 'icons8-refresh-64.png') # icon: https://icons8.com/icons/set/refresh
 
