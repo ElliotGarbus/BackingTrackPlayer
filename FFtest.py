b8bdb88f8be74239d303505f349b9f3cd8ada49f
ElliotGarbus <elliotg2@cox.net>
2020-12-13 21:51:04 -0700

    FFMPEG test subprocess call

---
+++ FFtest.py
@@ -0,0 +1,11 @@
+#  ffmpeg -y -i source.mp3 -filter:a atempo=1.25 output_125.mp3
+
+import subprocess
+
+ffmpeg_cmd = 'ffmpeg -y -i source.mp3 -filter:a atempo=1.25 output_125.mp3'.split()
+try:
+    result = subprocess.run(ffmpeg_cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL )
+except FileNotFoundError as fnfe:
+    print(fnfe)
+except subprocess.CalledProcessError as cpe:
+    print(cpe)
